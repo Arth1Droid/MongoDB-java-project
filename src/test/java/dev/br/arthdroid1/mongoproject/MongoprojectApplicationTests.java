1839ad54eb2307b011adca2223fe7475f62587a7
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-22 17:18:52 -0300

    Start Project

---
+++ src/test/java/dev/br/arthdroid1/mongoproject/MongoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.br.arthdroid1.mongoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
