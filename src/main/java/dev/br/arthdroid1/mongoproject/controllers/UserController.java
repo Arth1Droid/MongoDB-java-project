0e560bd3ab4a38b5713838bd2b4e663af2bc7fe8
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-24 13:54:35 -0300

    Create class UserController in controllers

---
+++ src/main/java/dev/br/arthdroid1/mongoproject/controllers/UserController.java
@@ -0,0 +1,29 @@
+package dev.br.arthdroid1.mongoproject.controllers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.br.arthdroid1.mongoproject.models.entities.User;
+import dev.br.arthdroid1.mongoproject.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
+
+	}
+}
