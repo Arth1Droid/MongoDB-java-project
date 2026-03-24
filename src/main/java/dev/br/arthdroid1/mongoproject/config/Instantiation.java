3aa906b01985f1ab934059bfbd7a962543316f9d
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-24 15:47:51 -0300

    Create Instantiation class to populate the database.

---
+++ src/main/java/dev/br/arthdroid1/mongoproject/config/Instantiation.java
@@ -0,0 +1,33 @@
+package dev.br.arthdroid1.mongoproject.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import dev.br.arthdroid1.mongoproject.models.entities.User;
+import dev.br.arthdroid1.mongoproject.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+		
+	}
+
+}
