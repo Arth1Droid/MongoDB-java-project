5cc437867e92047c43bbab7d03c213e9bd75377c
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-24 17:59:39 -0300

    Update UserService with CRUD functions

--- src/main/java/dev/br/arthdroid1/mongoproject/services/UserService.java
+++ src/main/java/dev/br/arthdroid1/mongoproject/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import dev.br.arthdroid1.mongoproject.models.entities.User;
 import dev.br.arthdroid1.mongoproject.repositories.UserRepository;
 import dev.br.arthdroid1.mongoproject.services.exceptions.ObjectNotFoundException;
+import dto.UserDTO;
 
 @Service
 public class UserService {
@@ -16,6 +17,9 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
 	public List<User> findAll(){
 		return repository.findAll();
 	}
@@ -23,6 +27,26 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+	}
+	
+	public User update(User user) {
+		User searchedUser = repository.findById(user.getId()).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		updatedData(searchedUser,user);
+		return repository.save(searchedUser);
+	}
+	
+	private void updatedData(User searchedUser, User user) {
+		searchedUser.setName(user.getName());
+		searchedUser.setEmail(user.getEmail());
+		
+	}
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
 	}
 
 }
