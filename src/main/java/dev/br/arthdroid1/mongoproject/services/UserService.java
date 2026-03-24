930743f18d2d56251805234e29f6c49e456d8b1f
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-24 16:26:54 -0300

    Update class UserService to use UserDTO

--- src/main/java/dev/br/arthdroid1/mongoproject/services/UserService.java
+++ src/main/java/dev/br/arthdroid1/mongoproject/services/UserService.java
@@ -1,12 +1,14 @@
 package dev.br.arthdroid1.mongoproject.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dev.br.arthdroid1.mongoproject.models.entities.User;
 import dev.br.arthdroid1.mongoproject.repositories.UserRepository;
+import dev.br.arthdroid1.mongoproject.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 
 }
