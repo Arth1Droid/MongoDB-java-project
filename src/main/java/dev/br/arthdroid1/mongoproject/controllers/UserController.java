cc25c1a1f59f5770b3eab73b22efe987252fcf8d
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-24 16:27:11 -0300

    Update class UserController to use UserDTO

--- src/main/java/dev/br/arthdroid1/mongoproject/controllers/UserController.java
+++ src/main/java/dev/br/arthdroid1/mongoproject/controllers/UserController.java
@@ -1,17 +1,17 @@
 package dev.br.arthdroid1.mongoproject.controllers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.br.arthdroid1.mongoproject.models.entities.User;
 import dev.br.arthdroid1.mongoproject.services.UserService;
+import dto.UserDTO;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -21,9 +21,18 @@ public class UserController {
 	private UserService userService;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = userService.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).toList();
+
+		return ResponseEntity.ok().body(listDTO);
+
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@RequestParam String id){
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 
 	}
 }
